5804f55ed2ed7541380868854dab94df515a701a
Santii05-C <santi.c.pgm@gmail.com>
2024-08-29 09:57:18 -0300

    first commit

---
+++ src/home.jsx
@@ -0,0 +1,15 @@
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+
+function Home() {
+  return (
+    <div>
+      {/* HEADER */}
+      <Header />
+      {/* HERO */}
+      <Hero />
+    </div>
+  );
+}
+
+export default Home;
