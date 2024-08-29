e6566815310ad0e1888aa557ce6e35ad8f3e9ee2
Santii05-C <santi.c.pgm@gmail.com>
2024-08-29 10:56:57 -0300

    last commit

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -8,9 +8,31 @@ import {
 
 function Search() {
   return (
-    <div>
+    <div className="p-2 md:p-5 bg-white rounded-md md:rounded-full flex-col md:flex md:flex-row gap-10 px-5 items-center w-[60%] ">
       <Select>
-        <SelectTrigger className="w-[180px]">
+        <SelectTrigger className=" outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <Select>
+        <SelectTrigger className=" outline-none md:border-none w-full shadow-none text-lg">
+          <SelectValue placeholder="Theme" />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem value="light">Light</SelectItem>
+          <SelectItem value="dark">Dark</SelectItem>
+          <SelectItem value="system">System</SelectItem>
+        </SelectContent>
+      </Select>
+
+      <Select>
+        <SelectTrigger className=" outline-none md:border-none w-full shadow-none text-lg">
           <SelectValue placeholder="Theme" />
         </SelectTrigger>
         <SelectContent>
