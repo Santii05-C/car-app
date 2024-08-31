3f9128affffe707f0256b5dd9685f7aae68d1b47
Santii05-C <santi.c.pgm@gmail.com>
2024-08-30 21:56:55 -0300

    last commit

--- src/add-listing/index.jsx
+++ src/add-listing/index.jsx
@@ -1,5 +1,24 @@
+import Header from "@/components/Header";
+
 function AddListing() {
-  return <div>AddListing</div>;
+  return (
+    <div>
+      <Header />
+      <div className="px-10 md:px-20 my-10">
+        <h2 className="font-bold text-4xl">Add New Listing</h2>
+
+        <form className="p-10 border rounded-xl mt-10">
+          {/* CAR DETAILS */}
+          <div className="">
+            <h2 className="font-medium text-xl mb-6">Car Details</h2>
+          </div>
+          {/* FEATURES LIST  */}
+
+          {/* CAR IMAGES */}
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default AddListing;
