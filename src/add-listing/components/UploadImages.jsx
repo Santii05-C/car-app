ecaf657cb737f49850e573a228ae392007afb131
Santii05-C <santi.c.pgm@gmail.com>
2024-09-02 16:47:37 -0300

    last commit

--- src/add-listing/components/UploadImages.jsx
+++ src/add-listing/components/UploadImages.jsx
@@ -12,13 +12,21 @@ function UploadImages() {
     }
   };
 
+  const onImageRemove = (image, index) => {
+    const result = selectedFilesList.filter((item) => item != image);
+    setSelectedFilesList(result);
+  };
+
   return (
     <div>
       <h2 className="font-medium text-xl my-3">Upload Car Images</h2>
       <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 gap-5">
         {selectedFilesList.map((image, index) => (
           <div key={index}>
-            <IoMdCloseCircle />
+            <IoMdCloseCircle
+              className="absolute m-2 text-lg text-white cursor-pointer"
+              onClick={() => onImageRemove(image, index)}
+            />
             <img
               src={URL.createObjectURL(image)}
               className="w-full h-[130px] object-cover rounded-xl"
