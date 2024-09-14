07d10794d6fa2212f23e7c7bc9039f303c0f7add
Santii05-C <santi.c.pgm@gmail.com>
2024-09-14 17:03:57 -0300

    last commit

--- src/listing-details/components/DetailsHeader.jsx
+++ src/listing-details/components/DetailsHeader.jsx
@@ -1,7 +1,15 @@
+import { HiCalendarDays } from "react-icons/hi2";
+
 function DetailsHeader({ carDetail }) {
   return (
     <div>
-      <h2 className="">{carDetail?.listingTitle}</h2>
+      <h2 className="font-bold text-3xl">{carDetail?.listingTitle}</h2>
+      <p className="text-sm">{carDetail?.tagline}</p>
+
+      <div className="flex gap-2 items-center bg-blue-50 rounded-full p-1 px-3">
+        <HiCalendarDays className="h-7 w-7 text-primary" />
+        <h2 className="text-primary text-sm">{carDetail?.year}</h2>
+      </div>
     </div>
   );
 }
