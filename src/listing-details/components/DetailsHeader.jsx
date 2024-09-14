e28bafddb957b7a82e435b71b005fe1de11f99ca
Santii05-C <santi.c.pgm@gmail.com>
2024-09-14 17:23:30 -0300

    last commit

--- src/listing-details/components/DetailsHeader.jsx
+++ src/listing-details/components/DetailsHeader.jsx
@@ -1,4 +1,7 @@
 import { HiCalendarDays } from "react-icons/hi2";
+import { BsSpeedometer2 } from "react-icons/bs";
+import { GiGearStickPattern } from "react-icons/gi";
+import { FaGasPump } from "react-icons/fa6";
 
 function DetailsHeader({ carDetail }) {
   return (
@@ -6,9 +9,23 @@ function DetailsHeader({ carDetail }) {
       <h2 className="font-bold text-3xl">{carDetail?.listingTitle}</h2>
       <p className="text-sm">{carDetail?.tagline}</p>
 
-      <div className="flex gap-2 items-center bg-blue-50 rounded-full p-1 px-3">
-        <HiCalendarDays className="h-7 w-7 text-primary" />
-        <h2 className="text-primary text-sm">{carDetail?.year}</h2>
+      <div className="flex gap-2 mt-3">
+        <div className="flex gap-2 items-center bg-blue-50 rounded-full p-1 px-3">
+          <HiCalendarDays className="h-7 w-7 text-primary" />
+          <h2 className="text-primary text-sm">{carDetail?.year}</h2>
+        </div>
+        <div className="flex gap-2 items-center bg-blue-50 rounded-full p-1 px-3">
+          <BsSpeedometer2 className="h-7 w-7 text-primary" />
+          <h2 className="text-primary text-sm">{carDetail?.mileage}</h2>
+        </div>
+        <div className="flex gap-2 items-center bg-blue-50 rounded-full p-1 px-3">
+          <GiGearStickPattern className="h-7 w-7 text-primary" />
+          <h2 className="text-primary text-sm">{carDetail?.transmission}</h2>
+        </div>
+        <div className="flex gap-2 items-center bg-blue-50 rounded-full p-1 px-3">
+          <FaGasPump className="h-7 w-7 text-primary" />
+          <h2 className="text-primary text-sm">{carDetail?.fuelType}</h2>
+        </div>
       </div>
     </div>
   );
