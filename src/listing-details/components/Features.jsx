733996c5df242b5da9ff7d22cd1c2d649149e1e0
Santii05-C <santi.c.pgm@gmail.com>
2024-09-15 12:34:29 -0300

    last commit

--- src/listing-details/components/Features.jsx
+++ src/listing-details/components/Features.jsx
@@ -6,15 +6,16 @@ function Features({ features }) {
       <h2 className="font-medium text-2xl">Features</h2>
 
       <div className="grid grid-cols-2 md:grid-cols-3 mt-5 lg:grid-cols-4 gap-7">
-        {Object.entries(features).map(([features, value, index]) => (
-          <div className="flex gap-2 items-center" key={index}>
-            <FaCheck className="text-lg p-1 rounded-full bg-blue-100 text-primary" />
-            <h2 className="">{features} </h2>
-          </div>
-        ))}
+        {features &&
+          Object.entries(features).map(([feature, value], index) => (
+            <div className="flex gap-2 items-center" key={index}>
+              <FaCheck className="text-lg p-1 rounded-full bg-blue-100 text-primary" />
+              <h2 className="">{feature}</h2>
+            </div>
+          ))}
       </div>
     </div>
   );
 }
-
+//5:23
 export default Features;
--- src/listing-details/components/Specification.jsx
+++ src/listing-details/components/Specification.jsx
@@ -4,21 +4,18 @@ import CarSpecification from "@/Shared/CarSpecification";
 function Specification({ carDetail }) {
   return (
     <div className="p-10 rounded-xl border shadow-md mt-7">
-      {CarSpecification.map((item, index) => (
-        <div className="mt-5 flex items-center justify-between" key={index}>
-          <h2 className="flex gap-2">
-            <IconField icon={item.icon} /> {item.label}
-          </h2>
-          <h2>{carDetail[item.name]}</h2>
-        </div>
-      ))}
-
-      {/* {carDetail?.lenght > 0 &&
-        carDetail.map((carItem, index) => (
-          <div className="" key={index}>
-            <IconField icon={CarSpecification[index]?.icon} />
+      {carDetail ? (
+        CarSpecification.map((item, index) => (
+          <div className="mt-5 flex items-center justify-between" key={index}>
+            <h2 className="flex gap-2">
+              <IconField icon={item?.icon} /> {item?.label}
+            </h2>
+            <h2>{carDetail?.[item?.name]}</h2>
           </div>
-        ))} */}
+        ))
+      ) : (
+        <div className="w-full h-[500px] rounded-xl bg-slate-200 animate-pulse "></div>
+      )}
     </div>
   );
 }
