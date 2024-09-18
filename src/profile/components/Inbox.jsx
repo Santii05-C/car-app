6c9665ff6e4d9a25eafbeb839e861b3ff3d330ce
Santii05-C <santi.c.pgm@gmail.com>
2024-09-18 17:47:21 -0300

    last commit

--- src/profile/components/Inbox.jsx
+++ src/profile/components/Inbox.jsx
@@ -1,11 +1,15 @@
+import { useEffect, useState } from "react";
 import { App as SendbirdApp, SendBirdProvider } from "@sendbird/uikit-react";
 import "@sendbird/uikit-react/dist/index.css";
 import { useUser } from "@clerk/clerk-react";
-import { useEffect, useState } from "react";
+import { GroupChannel } from "@sendbird/uikit-react/GroupChannel";
+import { GroupChannelList } from "@sendbird/uikit-react/GroupChannelList";
 
 function Inbox() {
   const { user } = useUser();
   const [userId, setUserId] = useState();
+  const [channelUrl, setChannelUrl] = useState();
+
   useEffect(() => {
     if (user) {
       const id = (user.primaryEmailAddress?.emailAddress).split("@")[0];
@@ -15,7 +19,7 @@ function Inbox() {
 
   return (
     <div>
-      <div style={{ width: "100vw", height: "100vh" }}>
+      <div style={{ width: "100%", height: "500px" }}>
         <SendBirdProvider
           appId={import.meta.env.VITE_SENDBIRD_APP_ID}
           userId={userId}
@@ -23,7 +27,20 @@ function Inbox() {
           profileUrl={user?.imageUrl}
           allowProfileEdit={true}
         >
-          {/* CHANNEL LIST */}
+          <div className="grid grid-cols-1 md:grid-cols-3 h-full">
+            {/* CHANNEL LIST */}
+            <div>
+              <GroupChannelList
+                onChannelSelect={(channel) => {
+                  setChannelUrl(channel?.url);
+                }}
+              />
+            </div>
+            {/* CHANNEL / MESSAGE AREA */}
+            <div className="md:col-span-2">
+              <GroupChannel channelUrl={channelUrl} />
+            </div>
+          </div>
         </SendBirdProvider>
       </div>
     </div>
