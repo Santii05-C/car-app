6bf6df805c0db051354914e204f92e5c09e58bfc
Santii05-C <santi.c.pgm@gmail.com>
2024-09-20 10:23:51 -0300

    last commit

--- src/components/CarItem.jsx
+++ src/components/CarItem.jsx
@@ -9,7 +9,7 @@ function CarItem({ car }) {
   return (
     <Link to={"/listing-details/" + car?.id}>
       <div className="relative rounded-xl bg-white border hover:shadow-lg cursor-pointer transition-shadow duration-200">
-        <h2 className="absolute top-2 left-2 bg-green-500 px-2 rounded-full text-sm text-white">
+        <h2 className="absolute top-2 left-2 bg-green-500 px-2 rounded-full text-xs text-white">
           New
         </h2>
         <img
@@ -19,28 +19,30 @@ function CarItem({ car }) {
           className="rounded-t-xl h-[180px] object-cover"
         />
         <div className="p-4">
-          <h2 className="font-bold text-black text-lg mb-2">
+          <h2 className="font-bold text-black text-lg mb-2 md:text-xl">
             {car?.listingTitle}
           </h2>
           <Separator />
-          <div className="grid grid-cols-3 gap-4 mt-5">
+          <div className="grid grid-cols-2 sm:grid-cols-3 gap-4 mt-5">
             <div className="flex flex-col items-center">
-              <LuFuel className="text-lg mb-2" />
-              <h2 className="text-sm text-center">{car?.mileage} Miles</h2>
+              <LuFuel className="text-lg mb-1" />
+              <h2 className="text-xs text-center">{car?.mileage} Millas</h2>
             </div>
             <div className="flex flex-col items-center">
-              <TbBrandSpeedtest className="text-lg mb-2" />
-              <h2 className="text-sm text-center">{car?.fuelType}</h2>
+              <TbBrandSpeedtest className="text-lg mb-1" />
+              <h2 className="text-xs text-center">{car?.fuelType}</h2>
             </div>
             <div className="flex flex-col items-center">
-              <GiGearStickPattern className="text-lg mb-2" />
-              <h2 className="text-sm text-center">{car?.transmission}</h2>
+              <GiGearStickPattern className="text-lg mb-1" />
+              <h2 className="text-xs text-center">{car?.transmission}</h2>
             </div>
           </div>
           <Separator className="my-2" />
-          <div className="flex items-center justify-between">
-            <h2 className="font-bold text-xl">{car?.sellingPrice}</h2>
-            <h2 className="text-primary text-sm flex gap-2 items-center">
+          <div className="flex flex-col sm:flex-row items-center justify-between">
+            <h2 className="font-bold text-lg sm:text-xl">
+              {car?.sellingPrice}
+            </h2>
+            <h2 className="text-primary text-xs flex gap-1 items-center">
               View Details <MdOpenInNew />
             </h2>
           </div>
